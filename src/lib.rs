f456deb653a35f97b33e0949e5f4ac7d2ad538ec
Tolya Korniltsev <anatoly.korniltsev@grafana.com>
2026-04-19 17:56:29 +1000

    rm criterion.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,5 @@
 // Copyright 2019 TiKV Project Authors. Licensed under Apache-2.0.
 
-//! pprof-rs is an integrated profiler for rust program.
-//!
-//! This crate provides a programable interface to start/stop/report a profiler
-//! dynamically. With the help of this crate, you can easily integrate a
-//! profiler into your rust program in a modern, convenient way.
-//!
-//! A sample usage is:
-//!
-//! ```rust
-//! let guard = pprof::ProfilerGuard::new(100).unwrap();
-//! ```
-//!
-//! Then you can read report from the guard:
-//!
-//! ```rust
-//! # let guard = pprof::ProfilerGuard::new(100).unwrap();
-//!if let Ok(report) = guard.report().build() {
-//!    println!("report: {:?}", &report);
-//!};
-//! ```
-//!
-//! More configuration can be passed through `ProfilerGuardBuilder`:
-//!
-//! ```rust
-//! let guard = pprof::ProfilerGuardBuilder::default().frequency(1000).blocklist(&["libc", "libgcc", "pthread", "vdso"]).build().unwrap();
-//! ```
-//!
-//! The frequency means the sampler frequency, and the `blocklist` means the
-//! profiler will ignore the sample whose first frame is from library containing
-//! these strings.
-//!
-//! Skipping `libc`, `libgcc` and `libpthread` could be a solution to the
-//! possible deadlock inside the `_Unwind_Backtrace`, and keep the signal
-//! safety. The dwarf information in "vdso" is incorrect in some distributions,
-//! so it's also suggested to skip it.
-//!
-//! You can find more details in
-//! [README.md](https://github.com/tikv/pprof-rs/blob/master/README.md)
 
 /// Define the MAX supported stack depth. TODO: make this variable mutable.
 #[cfg(feature = "large-depth")]
@@ -52,6 +14,7 @@ pub const MAX_DEPTH: usize = 128;
 /// Define the MAX supported thread name length. TODO: make this variable mutable.
 pub const MAX_THREAD_NAME: usize = 16;
 
+// todo replace with kindasafe
 mod addr_validate;
 
 mod backtrace;
@@ -94,5 +57,3 @@ pub mod protos {
     pub use self::profile::*;
 }
 
-#[cfg(feature = "criterion")]
-pub mod criterion;
